d2ac19709ef7217079f709aafa784e83cf946fbc
Christopher <cbstarke@hotmail.com>
2017-03-10 13:21:09 +0000

    ok

--- ProTypeScript/wwwroot/ts/A_Listing_01_22_The_IF_Statement.js
+++ ProTypeScript/wwwroot/ts/A_Listing_01_22_The_IF_Statement.js
@@ -1,1 +1,12 @@
+var isValid = true;
+var message;
+// Long-hand
+if (isValid) {
+    message = 'Okay';
+}
+else {
+    message = 'Failed';
+}
+// Conditional operator
+var message2 = isValid ? 'Okay' : 'Failed';
 //# sourceMappingURL=A_Listing_01_22_The_IF_Statement.js.map
